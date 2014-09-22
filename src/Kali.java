16b8981c153b0b8fa10c4a9022168cf6fc0cea39
Fauzan Adiima <fauzan.adiima@yahoo.com>
2014-09-22 15:47:42 +0700

    tugas prak RPLL

---
+++ src/Kali.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fauzan
+ */
+import java.util.Scanner;
+public class Kali {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Kalkulator Perkalian");
+        System.out.print("Masukkan Angka Pertama :");
+        int angka1 = input.nextInt();
+        System.out.print("Masukkan Angka Kedua :");
+        int angka2 = input.nextInt();
+        int hasil = angka1*angka2;
+        System.out.print("Hasil Perkaliannya adalah :" + hasil);
+    }
+}
